40ad049e796ede33a5cec26d8a454f6a7ae96285
Deivi Gutierrez Rojas <91471741+deivigutierrezmt@users.noreply.github.com>
2021-11-05 00:06:04 -0600

    Edit01

--- src/main/java/com/user/api/controller/DoctorController.java
+++ src/main/java/com/user/api/controller/DoctorController.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/Doctor")
-@CrossOrigin( origins = "*", methods = {RequestMethod.GET, RequestMethod.PUT ,RequestMethod.DELETE})
+@CrossOrigin( origins = "*", methods = {RequestMethod.GET,RequestMethod.POST, RequestMethod.PUT ,RequestMethod.DELETE})
 public class DoctorController {
     @Autowired
     private DoctorService  doctorservice;
